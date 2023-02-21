49d2b645e55808b224cee8726dfe397026db1b13
irishmaestro <70972101+IrishMaestro@users.noreply.github.com>
2023-02-21 15:23:22 -0600

    v0.1.1 -> added pull & list subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,71 @@
-use clap::Parser;
+use ansi_term::Color::Yellow;
+use clap::{arg, Command};
 
 pub mod nordvpn;
 pub mod protonvpn;
 pub mod surfshark;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    provider: String,
+const DESC: &str = env!("CARGO_PKG_DESCRIPTION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
+
+fn list_canaries() {
+    println!("{}", Yellow.bold().paint("nordvpn"));
+    println!("{}", Yellow.bold().paint("surfshark"));
+    println!("{}", Yellow.bold().paint("protonvpn"));
 }
 
-fn usage() {
-    println!("vpn providers offering canaries:");
-    println!("    surfshark");
-    println!("    nordvpn");
-    println!("    protonvpn");
+fn cli() -> Command {
+    Command::new("canaryspy")
+        .about(DESC)
+        .version(VERSION)
+        .author(AUTHOR)
+        .help_template(
+            "\
+{before-help}{name} {version}
+{author-with-newline}{about-with-newline}
+{usage-heading} {usage}
+
+{all-args}{after-help}
+",
+        )
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        // PULL
+        .subcommand(
+            Command::new("pull")
+                .short_flag('p')
+                .long_flag("pull")
+                .about("retrieve canary")
+                .arg(arg!(<VPN_PROVIDER>))
+                .arg_required_else_help(true),
+        )
+        // LIST
+        .subcommand(
+            Command::new("list")
+                .short_flag('l')
+                .long_flag("list")
+                .about("list canaries available")
+                .args_conflicts_with_subcommands(true),
+        )
 }
 
 fn main() {
-    let args = Args::parse();
-    match args.provider.as_str() {
-        "surfshark" => surfshark::canary(),
-        "nordvpn" => nordvpn::canary(),
-        "protonvpn" => protonvpn::canary(),
-        _ => usage(),
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("pull", pull_matches)) => {
+            match pull_matches
+                .get_one::<String>("VPN_PROVIDER")
+                .expect("required")
+                .as_str()
+            {
+                "nordvpn" => nordvpn::canary(),
+                "surfshark" => surfshark::canary(),
+                "protonvpn" => protonvpn::canary(),
+                _ => println!("provider not found. `canaryspy list` to list providers"),
+            }
+        }
+        Some(("list", _list_matches)) => list_canaries(),
+        _ => unreachable!(),
     }
 }
